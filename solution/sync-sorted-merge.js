5a4481aa47328e6af8404e8c5cfd9a4bbf00c9d3
knowlesc <knowlesc8@gmail.com>
2022-11-18 14:19:53 -0800

    Add some time complexity comments

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -7,13 +7,18 @@ module.exports = (logSources, printer) => {
   const pq = new MinPriorityQueue(({ log }) => log.date);
 
   // Get the earliest log from all log sources and insert into priority queue
+  // O(K) for the outer loop, O(K log K) total
   for (const source of logSources) {
     const log = source.pop();
+
+    // O(log K)
     pq.push({ log, source });
   }
 
+  // O(KN) for the outer loop, so O(KN * log K) total
   while (!pq.isEmpty()) {
     // pop the earliest log from the queue
+    // O(1) to get the root
     const { source, log } = pq.pop();
 
     printer.print(log);
@@ -22,6 +27,7 @@ module.exports = (logSources, printer) => {
     // the pq will contain no more entries than the # sources as we only fill one at a time
     if (!source.drained) {
       const log = source.pop();
+      // Still O(log K) as it will only ever hold 1 element per source
       if (log) pq.push({ log, source });
     }
   }
